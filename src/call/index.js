fae12051a067aea299d22c748df00a3242d744a6
ohnow <dennis@sumor.com>
2024-12-08 11:05:58 +0800

    fix proxy issue

--- src/call/index.js
+++ src/call/index.js
@@ -4,7 +4,13 @@ import processError from './processError.js'
 
 export default async (key, url, data) => {
   try {
-    const response = await axios.post(url, data, formatOptions(key))
+    const response = await axios({
+      proxy: false,
+      method: 'post',
+      url,
+      data,
+      ...formatOptions(key)
+    })
 
     return response.data
   } catch (error) {
